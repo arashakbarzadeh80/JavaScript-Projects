73e14cc32bfa85094f98c51bdbb9cbfc0d040c65
arashakbarzadeh80 <arashakbarzadeh80@gmail.com>
2026-09-10 19:58:23 -0700

    Basic JavaScript Projects Assignment

---
+++ Basic JavaScript Projects/Project1_expressions_alert/JS/main.js
@@ -0,0 +1,16 @@
+document.write("Hello, World! <br>"); //This is a string
+var A="Hello, World to every one <br>";
+document.write(A); 
+var B="This is a string"; //This is a string variable declaration
+window.alert(B); 
+document.write("Sara told me \"This is a string\"<br>"); //This is a string with double quotes
+document.write("This file is Arash\'s file!<br>"); //This is a string with an apostrophe
+document.write("This is a test for concatenate a string within the document." + "\"This is for Arash\'s training.\" <br>"); //This is a string concatenation
+var C="This is a test for concatenate a string within the document." + "\"This is for Arash\'s training.\" <br>"; //This is a variable declaration with a string concatenation
+document.write(C);
+var D="variable 1", E="variable 2", F="variable 3 <br>"; //This is a variable declaration with multiple variables
+document.write(D + "<br>");
+document.write(E + "<br>");
+document.write(F + "<br>");
+document.write(3+3 + "<br>"); //This is an addition operator
+document.write(3*3 + "<br>"); //This is a multiplication operator
