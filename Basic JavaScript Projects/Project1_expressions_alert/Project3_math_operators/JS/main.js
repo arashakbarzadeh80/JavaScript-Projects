936cf421e86808e8d27cab78ca0b28d3505df5d5
arashakbarzadeh80 <arashakbarzadeh80@gmail.com>
2026-09-13 23:48:26 -0700

    JavaScript operations

---
+++ Basic JavaScript Projects/Project1_expressions_alert/Project3_math_operators/JS/main.js
@@ -0,0 +1,45 @@
+// JavaScript addition 
+function additionFunction() {
+     number1 = 10; number2 = 20; 
+     document.getElementById("Math1").innerHTML = number1 + number2; 
+    }
+    // JavaScript subtraction 
+function subtractionFunction() {
+     number1 = 50; number2 = 25; 
+     document.getElementById("Math2").innerHTML = number1 - number2; 
+    }
+    // JavaScript multiplication
+function multiplicationFunction() {
+     number1 = 10; number2 = 20; 
+     document.getElementById("Math3").innerHTML = number1 * number2; 
+    }
+    // JavaScript division
+function divisionFunction() {
+     number1 = 50; number2 = 25; 
+     document.getElementById("Math4").innerHTML = number1 / number2; 
+    }
+    // JavaScript multiple math operations
+function mathOperations() {
+         result = (10 + 5) * 2 - 10 / 5; 
+         document.getElementById("Math5").innerHTML = result; 
+        }
+        // JavaScript modulus operator
+function modulusOperator() {
+         result = 25 % 6; 
+         document.getElementById("Math6").innerHTML = result; 
+        }
+        // JavaScript negation operator
+function negationOperator() {
+         result = -40; 
+         document.getElementById("Math7").innerHTML = result; 
+        }
+        // Increment and Decrement Operators
+var Y=14.5
+Y++
+document.write(Y) 
+var Z=21.5
+Z--
+document.write(Z)
+// JavaScript random number
+window.alert(Math.random() * 100);
+window.alert(Math.round(4.6));
