bb4a860b850afd4ff29c9af44b53b01c02e20b0a
arashakbarzadeh80 <arashakbarzadeh80@gmail.com>
2026-09-13 22:06:25 -0700

    New folders and files are added

---
+++ Basic JavaScript Projects/Project1_expressions_alert/Project2_functions/JS/main.js
@@ -0,0 +1,6 @@
+// Concatenating Strings function
+function myFunction() {
+var sentence = "I am learning JavaScript"; //cencatinating variable
+sentence += " from this course!"; //Using the += operator to add to variable
+document.getElementById("concatenate").innerHTML = sentence;  //using document.getElementById to display the result in HTML
+}
